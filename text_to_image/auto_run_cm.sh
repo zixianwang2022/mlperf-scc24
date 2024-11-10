5bf97978d275ce2322c64cb2bb5bf651a3aee683
zixian-wang-amd <Zixian.Wang@amd.com>
2024-11-10 15:46:25 -0600

    Updated coco accuracy with upstream dev

--- text_to_image/auto_run_cm.sh
+++ text_to_image/auto_run_cm.sh
@@ -64,3 +64,18 @@ cm run script --tags=run-mlperf,inference,_r4.1-dev,_short,_scc24-base \
    --quiet --precision=float16 \
    --adr.mlperf-implementation.tags=_branch.test,_repo.https://github.com/zixianwang2022/mlperf-scc24 --adr.mlperf-implementation.version=custom  --env.CM_GET_PLATFORM_DETAILS=no
    
+
+
+# Generate actual submission tree
+cm run script --tags=generate,inference,submission \
+   --clean \
+   --run-checker \
+   --tar=yes \
+   --env.CM_TAR_OUTFILE=submission.tar.gz \
+   --division=open \
+   --category=datacenter \
+   --env.CM_DETERMINE_MEMORY_CONFIGURATION=yes \
+   --run_style=test \
+   --adr.submission-checker.tags=_short-run \
+   --quiet \
+   --submitter=<Team Name>
